ac18efc9f87aa1062dcd980f8f03e707fe9e225a
William Kennedy <bill@ardanlabs.com>
2025-10-03 11:30:31 -0400

    created docling package

--- cmd/examples/example13/step1/main.go
+++ cmd/examples/example13/step1/main.go
@@ -1,22 +1,17 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"encoding/csv"
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"mime/multipart"
-	"net"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/ardanlabs/ai-training/foundation/client"
+	"github.com/ardanlabs/ai-training/foundation/docling"
 )
 
 var (
@@ -68,14 +63,28 @@ func run() error {
 
 	fmt.Println("\nExtract content from document")
 
-	data, err := docling(ctx, documentPath)
+	doc := docling.New(urlDocling)
+
+	fields := map[string]string{
+		"to_formats":                "md",
+		"include_images":            "false",
+		"table_mode":                "accurate",
+		"md_page_break_placeholder": "---",
+		"pdf_backend":               "dlparse_v4",
+		"image_export_mode":         "placeholder",
+	}
+
+	data, err := doc.ConvertFile(ctx, documentPath, fields)
 	if err != nil {
 		return fmt.Errorf("docling: %w", err)
 	}
 
+	fmt.Println("\nExtracted content")
+	fmt.Printf("\u001b[92m%s\u001b[0m", data)
+
 	// -------------------------------------------------------------------------
 
-	fmt.Println("Process against the LLM")
+	fmt.Println("\nProcess against the LLM")
 
 	csvData, err := ollama(ctx, data)
 	if err != nil {
@@ -93,7 +102,7 @@ func run() error {
 	}
 
 	for _, record := range records {
-		fmt.Println(record)
+		fmt.Printf("\u001b[93m%s\u001b[0m", record)
 	}
 
 	return nil
@@ -166,87 +175,4 @@ func ollama(ctx context.Context, data string) (string, error) {
 	}
 
 	return csvData.String(), nil
-}
-
-func docling(ctx context.Context, file string) (string, error) {
-	cln := http.Client{
-		Transport: &http.Transport{
-			Proxy: http.ProxyFromEnvironment,
-			DialContext: (&net.Dialer{
-				Timeout:   10 * time.Second,
-				KeepAlive: 15 * time.Second,
-				DualStack: true,
-			}).DialContext,
-			ForceAttemptHTTP2:     true,
-			MaxIdleConns:          100,
-			IdleConnTimeout:       90 * time.Second,
-			TLSHandshakeTimeout:   10 * time.Second,
-			ExpectContinueTimeout: 1 * time.Second,
-		},
-	}
-
-	var b bytes.Buffer
-	writer := multipart.NewWriter(&b)
-
-	fileWriter, err := writer.CreateFormFile("files", file)
-	if err != nil {
-		return "", fmt.Errorf("create form file: %w", err)
-	}
-
-	f, err := os.Open(file)
-	if err != nil {
-		return "", fmt.Errorf("open file: %w", err)
-	}
-	defer f.Close()
-
-	if _, err := io.Copy(fileWriter, f); err != nil {
-		return "", fmt.Errorf("copy file: %w", err)
-	}
-
-	writer.WriteField("to_formats", "md")
-	writer.WriteField("include_images", "false")
-	writer.WriteField("table_mode", "accurate")
-	writer.WriteField("md_page_break_placeholder", "---")
-	writer.WriteField("pdf_backend", "dlparse_v4")
-	writer.WriteField("image_export_mode", "placeholder")
-
-	if err := writer.Close(); err != nil {
-		return "", fmt.Errorf("close writer: %w", err)
-	}
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, urlDocling, &b)
-	if err != nil {
-		return "", fmt.Errorf("create request error: %w", err)
-	}
-
-	req.Header.Set("Content-Type", writer.FormDataContentType())
-
-	resp, err := cln.Do(req)
-	if err != nil {
-		return "", fmt.Errorf("do request: %w", err)
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", fmt.Errorf("read response: %w", err)
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("unexpected status: %d, body: %s", resp.StatusCode, body)
-	}
-
-	// -------------------------------------------------------------------------
-
-	var data struct {
-		Document struct {
-			MDContent string `json:"md_content"`
-		} `json:"document"`
-	}
-
-	if err := json.Unmarshal(body, &data); err != nil {
-		return "", fmt.Errorf("unmarshal: %w", err)
-	}
-
-	return data.Document.MDContent, nil
 }

e08e3bdd38cd0196c8a3d850a766434a531da62e
Miki Tebeka <miki@353solutions.com>
2025-12-16 17:13:39 +0200

    Fix path to image file

--- cmd/examples/example13/step2/main.go
+++ cmd/examples/example13/step2/main.go
@@ -26,7 +26,7 @@ import (
 const (
 	modelURL       = "https://huggingface.co/ggml-org/Qwen2.5-VL-3B-Instruct-GGUF/resolve/main/Qwen2.5-VL-3B-Instruct-Q8_0.gguf"
 	projURL        = "https://huggingface.co/ggml-org/Qwen2.5-VL-3B-Instruct-GGUF/resolve/main/mmproj-Qwen2.5-VL-3B-Instruct-Q8_0.gguf"
-	imageFile      = "examples/samples/giraffe.jpg"
+	imageFile      = "zarf/samples/gallery/giraffe.jpg"
 	modelInstances = 1
 )
 
